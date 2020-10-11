7da3b079eb4cca48f9e8621cbc5b35b1afae0e83
HenryMatias <matias.seppanen@gmail.com>
2020-10-11 10:31:39 +0300

    Now admin has an access to edit all the custom posts in WordPress

--- functions.php
+++ functions.php
@@ -90,8 +90,11 @@
 
     function receptini_create_custom_user() {
 
+        // This part creates a role called Recipe App user and 
+        // he will be given access to make changes only to
+        // custom posts in WordPress 
+        
         add_role('recipeuser', 'Reseptini käyttäjä');
-
         $app_user = get_role('recipeuser');
         $app_user->add_cap('read');
         $app_user->add_cap('edit_recipe');
@@ -107,7 +110,26 @@
         //$app_user->add_cap('edit_others_shoplist');
         //$app_user->add_cap('edit_others_shoplists');
 
-    }
+
+        // This part of a code gives administrator an access to
+        // handle all the posts that are been sent to custom
+        // posts in Wordpress
+
+        $admin = get_role('administrator');
+        $admin->add_cap('edit_recipe');
+        $admin->add_cap('edit_recipes');
+        $admin->add_cap('edit_others_recipe');
+        $admin->add_cap('edit_others_recipes');
+        $admin->add_cap('edit_setting');
+        $admin->add_cap('edit_settings');
+        $admin->add_cap('edit_others_setting');
+        $admin->add_cap('edit_others_settings');
+        $admin->add_cap('edit_shoplist');
+        $admin->add_cap('edit_shoplists');
+        $admin->add_cap('edit_others_shoplist');
+        $admin->add_cap('edit_others_shoplists');
+
+    };
 
     add_action('init', 'receptini_create_custom_user');
 
