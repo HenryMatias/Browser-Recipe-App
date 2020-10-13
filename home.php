c63490a28eedfffd2f114be7f58ae9bd1941e06d
HenryMatias <matias.seppanen@gmail.com>
2020-10-13 21:42:39 +0300

    Created frontpage

---
+++ home.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+
+<div id="section1">
+    <div id="section1-right-background"></div>
+    <div class="container" id="frontpage-header">
+    <img src="<?php echo get_template_directory_uri(); ?>/images/phone_header.png" alt="">
+        <div class="row">
+            <div class="col-md-6" id="frontpage-header-left">
+                <div class="heading">
+                    <h1>Reseptini</h1>
+                    <p>versio: 1.0.0</p>
+                </div> 
+            </div>
+            <div class="col-md-6" id="frontpage-header-right">
+            </div>
+        </div>
+    </div>
+</div>
