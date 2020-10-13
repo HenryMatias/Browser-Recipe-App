63ab182c80ebd9caa3af1e0e6c77e7371de9f6e9
HenryMatias <matias.seppanen@gmail.com>
2020-10-13 21:45:53 +0300

    Created header file for homepage header

---
+++ headers/header-home.php
@@ -0,0 +1,17 @@
+<body onload="settingCheckbox();" id="body-home">
+<header id="header-home">
+
+<div id="header-home">
+    <div class="container" id="header-home-nav">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/logo_purple.png" alt="">
+            </a>
+            <div id="login-input">
+                <a href="#">Kirjaudu</a>
+            </div>
+        </nav>
+    </div>
+</div>
+
+</header>
