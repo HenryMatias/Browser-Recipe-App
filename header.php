ebf0ab21dcafbe3aec02cd16c3890ce6be102bbd
HenryMatias <matias.seppanen@gmail.com>
2020-10-13 21:42:15 +0300

    Made two different headers and made own files for them

--- header.php
+++ header.php
@@ -9,34 +9,23 @@
     <?php wp_head(); ?>
 
 </head>
-<body onload="settingCheckbox();">
-<header>
 
 <?php
     include 'assets/sections/color-adjust.php';
-?>
+
+    // This gets the current pages' slug
+    global $post;
+    $post_slug = $post->post_name;
 
 
-<?php
-    $args = array(
-    'post_type' => 'setting',
-    'posts_per_page' => 1,
-    'orderby' => 'date',
-    'order' => 'ASC',
-    );
-    $settings = new WP_Query($args);
-    while ($settings->have_posts()) {
-    $settings->the_post();
-?>
-<div class="header-one" style="background-image: url(<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>)" id="headeroneid">
-<?php
+    if($post_slug == 'application') {
+
+        include 'headers/header-app.php';
+
+    } else {
+
+        include 'headers/header-home.php';
+
     }
-    wp_reset_query();
-?>    
-    <div class="logo">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/logo_purple.png" alt="">
-    </div>
-    <p class="editor" id="contentheading" style="color: <?php echo "$color1" ?>;">Reseptini</p>
-</div>
 
-</header>
+?>
