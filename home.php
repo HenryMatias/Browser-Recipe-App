015758ffc308d145aae133c967ce0f901b60d2e3
HenryMatias <matias.seppanen@gmail.com>
2020-10-20 16:07:41 +0300

    Created frontpage

--- home.php
+++ home.php
@@ -12,7 +12,305 @@
                 </div> 
             </div>
             <div class="col-md-6" id="frontpage-header-right">
+                <div class="header-right-cta">
+                    <p>
+                        Tutustu nyt
+                    </p>
+                    <a href="#">
+                        <i class="far fa-arrow-alt-circle-right"></i>
+                    </a>
+                </div>
             </div>
         </div>
+    </div>
+</div>
+<div id="section2">
+    <div id="section2-right-background"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col1">
+                <div class="mobile">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/phone_preview1.png" alt="">
+                </div>
+                <div class="mobile-text">
+                    <div class="heading">
+                        <h3><span>
+                        Rakastamme
+                        </span>
+                        <br>
+                        Puhelimia ja kokemusta</h3>
+                    </div>
+                    <div class="icons">
+                        <div>
+                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-phone" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M11 1H5a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z"/>
+                                <path fill-rule="evenodd" d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+                            </svg>
+                        </div>
+                        <div>
+                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-laptop" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M13.5 3h-11a.5.5 0 0 0-.5.5V11h12V3.5a.5.5 0 0 0-.5-.5zm-11-1A1.5 1.5 0 0 0 1 3.5V12h14V3.5A1.5 1.5 0 0 0 13.5 2h-11z"/>
+                                <path d="M0 12h16v.5a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 12.5V12z"/>
+                            </svg>
+                        </div>
+                        <div>
+                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-tablet" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M12 1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4z"/>
+                                <path fill-rule="evenodd" d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+                            </svg>
+                        </div>
+                    </div>
+                    <div class="content">
+                        <p>
+                            Toistaiseksi palvelumme toimii vain ja ainoastaan puhelimilla, mutta verkkosivumme skaalautuvat kaikille laitteille. Koska palvelu on toteutettu harrastus mielessä, tulee sivujen sisältö kokemaan muutoksia ajan saatossa parantaaksemme palvelun ja kokemuksen laatua.
+                        </p>
+                        <p>
+                            Koska palvelu on toteutettu puhtaasti käyttäjien ehdoilla, kuulemme mieluusti kehitysehdotuksia palvelun laadun ja kokemuksen parantamiseksi!
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col2">
+                <div class="mobile-text">
+                    <h2><span>
+                    Tallenna
+                    </span>
+                    <br>
+                    Reseptisi ja kauppalista</h2>
+                    <ul class="nav nav-tabs" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active" id="a-tab" data-toggle="tab" href="#a-content" role="tab" aria-controls="a" aria-selected="true">
+                                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-book" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M1 2.828v9.923c.918-.35 2.107-.692 3.287-.81 1.094-.111 2.278-.039 3.213.492V2.687c-.654-.689-1.782-.886-3.112-.752-1.234.124-2.503.523-3.388.893zm7.5-.141v9.746c.935-.53 2.12-.603 3.213-.493 1.18.12 2.37.461 3.287.811V2.828c-.885-.37-2.154-.769-3.388-.893-1.33-.134-2.458.063-3.112.752zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
+                                </svg>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="b-tab" data-toggle="tab" href="#b-content" role="tab" aria-controls="b" aria-selected="false">
+                                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-cart" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+                                </svg>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="c-tab" data-toggle="tab" href="#c-content" role="tab" aria-controls="c" aria-selected="false">
+                                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-brush" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M15.825.12a.5.5 0 0 1 .132.584c-1.53 3.43-4.743 8.17-7.095 10.64a6.067 6.067 0 0 1-2.373 1.534c-.018.227-.06.538-.16.868-.201.659-.667 1.479-1.708 1.74a8.117 8.117 0 0 1-3.078.132 3.658 3.658 0 0 1-.563-.135 1.382 1.382 0 0 1-.465-.247.714.714 0 0 1-.204-.288.622.622 0 0 1 .004-.443c.095-.245.316-.38.461-.452.393-.197.625-.453.867-.826.094-.144.184-.297.287-.472l.117-.198c.151-.255.326-.54.546-.848.528-.739 1.2-.925 1.746-.896.126.007.243.025.348.048.062-.172.142-.38.238-.608.261-.619.658-1.419 1.187-2.069 2.175-2.67 6.18-6.206 9.117-8.104a.5.5 0 0 1 .596.04zM4.705 11.912a1.23 1.23 0 0 0-.419-.1c-.247-.013-.574.05-.88.479a11.01 11.01 0 0 0-.5.777l-.104.177c-.107.181-.213.362-.32.528-.206.317-.438.61-.76.861a7.127 7.127 0 0 0 2.657-.12c.559-.139.843-.569.993-1.06a3.121 3.121 0 0 0 .126-.75l-.793-.792zm1.44.026c.12-.04.277-.1.458-.183a5.068 5.068 0 0 0 1.535-1.1c1.9-1.996 4.412-5.57 6.052-8.631-2.591 1.927-5.566 4.66-7.302 6.792-.442.543-.796 1.243-1.042 1.826a11.507 11.507 0 0 0-.276.721l.575.575zm-4.973 3.04l.007-.005a.031.031 0 0 1-.007.004zm3.582-3.043l.002.001h-.002z"/>
+                                </svg>
+                            </a>
+                        </li>
+                    </ul>
+                    <div class="tab-content">
+                        <div class="tab-pane fade show active" id="a-content" role="tabpanel" aria-labelledby="a-tab">
+                            <p>
+                            <b>Reseptini.com</b> on kuin oma, henkilökohtainen keittokirja, johon voit tallentaa omat reseptit talteen tulevia ruoanlaittohetkiä varten. Reseptini-ohjelma luo sinulle luettelon kaikista tallentamistasi resepteistäsi, ja voit halutessasi suodattaa tulokset suolaisiin tai makeisiin ohjeisiin.
+                            </p>
+                            <p>
+                            Reseptien lisääminen on helppoa ja vaivatonta; listaa vain tarvittavat ainekset sekä ohjeet kyseisen ruoan valmistukseen ja reseptisi on valmis! Voit jalostaa ruoasta entistä paremman muokkaamalla tallentaamaasi reseptiäsi milloin tahansa.    
+                            </p>
+                        </div>
+                        <div class="tab-pane fade" id="b-content" role="tabpanel" aria-labelledby="b-tab">
+                            <p>
+                            Resepteistä voit lisätä tarvitsemasi raaka-aineet suoraan henkilökohtaiseen ostoslistaasi, missä kaikki tarvitemasi tuotteet pysyvät tallessa seuraavaa kaupassakäyntiä varten.
+                            </p>
+                        </div>
+                        <div class="tab-pane fade" id="c-content" role="tabpanel" aria-labelledby="c-tab">
+                            <p>
+                            Jotta Reseptini olisi juuri sinun näköinen, voit halutessasi valita itsellesi vaihtoehtoisen taustakuvan sovellukseesi. Tämän lisäksi sinulla on mahdollisuus valita kaksi lempiväriäsi, jolloin palvelun tekstit ja kuvakkeet vaihtavat väriä sinun asettamiesi värien mukaiseksi
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="mobile">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/phone_preview2.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="section3">
+    <div class="container">
+        <div class="heading" id="headingarea">
+            <h4 id="moveheading">
+                Reseptini - Reseptini - Reseptini - Reseptini - Reseptini - Reseptini - Reseptini
+            </h4>
+        </div>
+        <script>
+            var windowHeight =  Math.max(document.documentElement.clientHeight, window.innerHeight || 0),
+                lastTop;
+
+            window.addEventListener('scroll', function(event) {
+            var movingtitle = document.getElementById('headingarea'),
+                top = movingtitle.getBoundingClientRect().top,
+                offset = top - windowHeight;
+                
+                if (offset > 0) {
+                    movingtitle.classList.remove('choochoo');
+                    return;
+                }
+                
+                if (top < windowHeight / 2 && top > lastTop) {
+                    movingtitle.classList.remove('choochoo');
+                }
+                
+                if (movingtitle.className.indexOf('choochoo') === -1 && top < lastTop) {
+                    movingtitle.classList.add('choochoo');
+                }
+                
+                lastTop = top;
+                
+            });
+        </script>
+        <div class="deco-circle-1">
+            <div class="outer"></div>
+            <div class="inner"></div>
+        </div>
+        <div class="deco-circle-2">
+            <div class="outer"></div>
+            <div class="inner"></div>
+        </div>
+        <div class="deco-circle-3">
+            <div class="outer"></div>
+            <div class="inner"></div>
+        </div>
+        <div class="deco-circle-4">
+            <div class="outer"></div>
+        </div>
+        <div class="background-circle-1"></div>
+        <div class="background-circle-2"></div>
+        <div class="circle-buttons" id="circlebuttons">
+            <div class="right">
+                <a href="#carouselExampleIndicators" role="button" data-slide="next" id="nextslide">
+                    <svg>
+                    <path d="m20,200 a15,3 0 0 1 170,00" id=path style=opacity:0 />
+                    <text><textPath xlink:href=#path>
+                        SEURAAVA
+                    </textPath></text>
+                    </svg>
+                </a>
+            </div>
+            <script>
+
+                document.getElementById("nextslide").addEventListener("click", increaseSlide);
+                function increaseSlide() {
+                    document.getElementById("circlebuttons").style.transform = "rotate(360deg)"
+                    document.getElementById("circlebuttons").style.transitionDuration = "0.8s";
+                    setTimeout( () => {
+                        document.getElementById("circlebuttons").style.transform = "rotate(0deg)";
+                        document.getElementById("circlebuttons").style.transitionDuration = "0s";
+                    }, 799);
+                };
+
+            </script>
+            <div class="left">
+                <a href="#carouselExampleIndicators" role="button" data-slide="prev" id="prevslide">
+                    <svg>
+                    <path d="M 45 0 Q 4 62 42 200" id=path2 style=opacity:0 />
+                    <text><textPath xlink:href=#path2>
+                        EDELLINEN
+                    </textPath></text>
+                    </svg>
+                </a>
+            </div>
+            <script>
+
+                document.getElementById("prevslide").addEventListener("click", decreaseSlide);
+                function decreaseSlide() {
+                    document.getElementById("circlebuttons").style.transform = "rotate(-360deg)"
+                    document.getElementById("circlebuttons").style.transitionDuration = "0.8s";
+                    setTimeout( () => {
+                        document.getElementById("circlebuttons").style.transform = "rotate(0deg)";
+                        document.getElementById("circlebuttons").style.transitionDuration = "0s";
+                    }, 799);
+                };
+
+            </script>
+        </div>
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/phone_preview1.png" class="d-block w-100" src="..." alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/phone_preview2.png" class="d-block w-100" src="..." alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/phone_preview3.png" class="d-block w-100" src="..." alt="Third slide">
+                </div>
+                <div class="carousel-item">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/phone_preview4.png" class="d-block w-100" src="..." alt="Fourth slide">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="section4">
+    <div class="container">
+        <div class="discussion">
+            <div class="bubble ask">
+                <p>
+                Mitä tälläinen ohjelman käyttäminen mahtaa minulle maksaa?
+                </p>
+            </div>
+            <div class="bubble answer">
+                <p>
+                Ei yhtikäs mitään! Reseptini on täysin ilmainen, eikä se vaadi sinulta muuta kuin sähköpostiosoitetta rekisteröitymistä varten.
+                </p>
+            </div>
+            <div class="bubble ask">
+                <p>
+                No mitäs se Reseptini tästä sitten hyötyy?
+                </p>
+            </div>
+            <div class="bubble answer">
+                <p>
+                Tarkoituksemme ei ole hyötyä tästä millään tavoin. Palvelu on maksuton eikä itsestään ole tarvetta luovuttaa minkäänlaisia tietoja niin halutessaan. Sähköpostiosoitettakin tarvitset vain rekisteröitymiseen. Niin, eikä tarvii muuten pelätä että me alettaisiin lähettelemään mainoksia sähköpostiisi. Se on vaan siihen rekisteröitymiseen.
+                </p>
+            </div>
+            <div class="bubble ask">
+                <p>
+                Mistä tämä sivusto on saanut alkunsa, millainen homma?
+                </p>
+            </div>
+            <div class="bubble answer">
+                <p>
+                Allekirjoittaneelle tultiin esittämään pyyntö omasta sovelluksesta, johon voisi tallentaa omat reseptit ja kauppalistan. Koska etsin vielä ensimmäistä työpaikkaa juniorina tai harkkarina verkkosivujen tekijänä, päätin toteuttaa sivut siten, että niistä voisi hyötyä kaikki.
+                </p>
+            </div>
+            <div class="bubble answer">
+                <p>
+                Psst! Jos tiedät paikan jossa voisin oikeasti päästä alalle sisään niin minut saa ilmiantaa!
+                </p>
+            </div>
+        </div>
+        <div class="send-question">
+            <div class="input-area">
+                <input type="text" placeholder="Yks juttu tuli mieleen...">
+                <button type="send">
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-right-circle-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-11.5.5a.5.5 0 0 1 0-1h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5z"/>
+                    </svg>
+                </button>
+            </div>
+        </div>
+        <div class="background-image"></div>
+    </div>
+</div>
+<div id="section5">
+    <div id="section5-middle-background"></div>
+    <div class="container">
+        <h3>Kiinnostuitko?</h3>
+        <p>Tulossa Pian!<br>Sivut avautuvat viimeistään 01.12.2020</p>
+    </div>
+</div>
+<div id="section6">
+    <div class="container">
+        
     </div>
 </div>
