43f5339def953b1474a8f46cd4e0402ddd708336
HenryMatias <matias.seppanen@gmail.com>
2020-10-22 22:02:19 +0300

    automatic changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,50 +7,70 @@
     }
 
 
-    // Tämä funktio vaihtaa näkymää reseptien ja ostoslistan välillä
+    function settingCheckbox() {
+        var checkBox = document.getElementById("maintheme");
+        var secondTheme = document.getElementById("secondtheme");
+        var slider = document.getElementById("themeslider");
+        var maintheme = document.getElementById("activetheme");
+        if (checkBox.checked == false){
+            secondTheme.style.opacity = "1";
+            secondTheme.style.maxHeight = "200px";
+            secondTheme.style.overflow = "visible";
+            secondTheme.style.padding = "20px 0px 10px 0px";
+            secondTheme.style.transition = "0.4s";
+            slider.style.paddingBottom = "10px";
+            maintheme.setAttribute("value", "unchecked");
+            
+        } else {
+            secondTheme.style.opacity = "0";
+            secondTheme.style.maxHeight = "0px";
+            secondTheme.style.overflow = "hidden";
+            secondTheme.style.padding = "0px";
+            slider.style.paddingBottom = "0px"
+            maintheme.setAttribute("value", "checked");
+        }
+        return;
+      };
 
-    
-    function toShoplist() {
+        (function ($, root, undefined) {
+	
+            $(function () {
+                
+                'use strict';
+                
+                // DOM ready, take it away
+
+                $(".remove").ajaxForm({
+                    url: '#recipeareaid',
+                    type: 'post',
+                    data: { 
+                        action: 'singular_delete'
+                        },
+                });
+
+                /*
+                $(".salty").ajaxForm({
+                    url: '#accordion',
+                    type: 'post',
+                    data: { action: 'salty_recipe_fetch' },
+                    success: function(data) {
+                        jQuery('#accordion').html( data );
+                }
+                });
+                */ 
+                
+            });
+
+            
+            
+        })(jQuery, this);
+
+
         
-        document.getElementById("pagecontent").style.transitionDuration = "0.4s";
-        document.getElementById("pagecontent").style.marginLeft = "0vh";
-        document.getElementById("pagecontent").style.marginRight = "10vw";
-        document.getElementById("pagecontent").style.borderRadius = "0px 30px 30px 0px";
-        document.getElementById("contentheading").style.transitionDuration = "0.5s";
-        document.getElementById("contentheading").style.color = colornew2;
-        document.getElementById("contentheading").style.right = "43%";
-        document.getElementById("contentheading").innerHTML = "Ostoslista";
-        document.getElementById("modalbutton").setAttribute("data-target", "#newlistitem");
-        document.getElementById("modalbutton").style.transitionDuration = "0.7s";
-        document.getElementById("modalbutton").style.transform = "rotate(180deg)";
-        document.getElementById("modalbutton").style.backgroundColor = colornew2;
-        document.getElementById("all-food-filter").style.transform = "translateY(60px)";
-        document.getElementById("all-food-filter").style.transitionDuration = "0.5s";
-        document.getElementById("salty-food-filter").style.transform = "translateY(60px)";
-        document.getElementById("salty-food-filter").style.transitionDuration = "0.5s";
-        document.getElementById("sweet-food-filter").style.transform = "translateY(60px)";
-        document.getElementById("sweet-food-filter").style.transitionDuration = "0.5s";
-        document.getElementById("delete-all-shoplist").style.transform = "translateY(0px)";
-        document.getElementById("delete-all-shoplist").style.transitionDuration = "0.5s";
-        document.getElementById("searchcontent").style.transitionDuration = "0.5s";
-        document.getElementById("searchcontent").style.transform = "translateX(120%)";
-        document.getElementById("maincontent").style.transitionDuration = "0.5s";
-        document.getElementById("maincontent").style.borderRadius = "0px 50px 0px 0px";
-        document.getElementById("maincontent").style.opacity = "1";
-        document.getElementById("maincontent").style.pointerEvents = "auto";
-        document.getElementById("maincontent").style.height = "auto";
-        document.getElementById("headeroneid").style.transitionDuration = "0.5s";
-        document.getElementById("headeroneid").style.height = "36vh";
-        document.getElementById("headeroneid").style.padding = "30px 0px 0px 30px";
-        document.getElementById("settings").style.transitionDuration = "0.5s";
-        document.getElementById("settings").style.opacity = "0";
-        document.getElementById("settings").style.top = "100%";
-        document.getElementById("settings").style.display = "none";
-    }
-    
 
+// To recipes in application menu
+document.getElementById("etusivu-tab").addEventListener("click", toRecipes);
     function toRecipes() {
-
         document.getElementById("pagecontent").style.transitionDuration = "0.4s";
         document.getElementById("pagecontent").style.marginLeft = "10vw";
         document.getElementById("pagecontent").style.marginRight = "0vw";
@@ -85,10 +105,52 @@
         document.getElementById("settings").style.opacity = "0";
         document.getElementById("settings").style.top = "100%";
         document.getElementById("settings").style.display = "none";
-    }
+    };
 
+
+// To shoplist in application menu
+document.getElementById("ostoslista-tab").addEventListener("click", toShoplist);
+    function toShoplist() {
+        document.getElementById("pagecontent").style.transitionDuration = "0.4s";
+        document.getElementById("pagecontent").style.marginLeft = "0vh";
+        document.getElementById("pagecontent").style.marginRight = "10vw";
+        document.getElementById("pagecontent").style.borderRadius = "0px 30px 30px 0px";
+        document.getElementById("contentheading").style.transitionDuration = "0.5s";
+        document.getElementById("contentheading").style.color = colornew2;
+        document.getElementById("contentheading").style.right = "43%";
+        document.getElementById("contentheading").innerHTML = "Ostoslista";
+        document.getElementById("modalbutton").setAttribute("data-target", "#newlistitem");
+        document.getElementById("modalbutton").style.transitionDuration = "0.7s";
+        document.getElementById("modalbutton").style.transform = "rotate(180deg)";
+        document.getElementById("modalbutton").style.backgroundColor = colornew2;
+        document.getElementById("all-food-filter").style.transform = "translateY(60px)";
+        document.getElementById("all-food-filter").style.transitionDuration = "0.5s";
+        document.getElementById("salty-food-filter").style.transform = "translateY(60px)";
+        document.getElementById("salty-food-filter").style.transitionDuration = "0.5s";
+        document.getElementById("sweet-food-filter").style.transform = "translateY(60px)";
+        document.getElementById("sweet-food-filter").style.transitionDuration = "0.5s";
+        document.getElementById("delete-all-shoplist").style.transform = "translateY(0px)";
+        document.getElementById("delete-all-shoplist").style.transitionDuration = "0.5s";
+        document.getElementById("searchcontent").style.transitionDuration = "0.5s";
+        document.getElementById("searchcontent").style.transform = "translateX(120%)";
+        document.getElementById("maincontent").style.transitionDuration = "0.5s";
+        document.getElementById("maincontent").style.borderRadius = "0px 50px 0px 0px";
+        document.getElementById("maincontent").style.opacity = "1";
+        document.getElementById("maincontent").style.pointerEvents = "auto";
+        document.getElementById("maincontent").style.height = "auto";
+        document.getElementById("headeroneid").style.transitionDuration = "0.5s";
+        document.getElementById("headeroneid").style.height = "36vh";
+        document.getElementById("headeroneid").style.padding = "30px 0px 0px 30px";
+        document.getElementById("settings").style.transitionDuration = "0.5s";
+        document.getElementById("settings").style.opacity = "0";
+        document.getElementById("settings").style.top = "100%";
+        document.getElementById("settings").style.display = "none";
+    };
+
+
+// To search in application menu
+document.getElementById("hae-tab").addEventListener("click", toSearch);
     function toSearch() {
-
         document.getElementById("pagecontent").style.transitionDuration = "0.4s";
         document.getElementById("pagecontent").style.marginLeft = "10vw";
         document.getElementById("pagecontent").style.marginRight = "0vw";
@@ -123,10 +185,12 @@
         document.getElementById("settings").style.opacity = "0";
         document.getElementById("settings").style.top = "100%";
         document.getElementById("settings").style.display = "none";
-    }
+    };
 
+
+// To shoplist in application menu
+document.getElementById("asetukset-tab").addEventListener("click", toSettings);
     function toSettings() {
-
         document.getElementById("headeroneid").style.transitionDuration = "0.2s";
         document.getElementById("headeroneid").style.height = "0vh";
         document.getElementById("headeroneid").style.padding = "0px";
@@ -140,65 +204,8 @@
         document.getElementById("settings").style.opacity = "1";
         document.getElementById("settings").style.top = "0%";
         document.getElementById("settings").style.display = "block";
-
-    }
-
-    function settingCheckbox() {
-        var checkBox = document.getElementById("maintheme");
-        var secondTheme = document.getElementById("secondtheme");
-        var slider = document.getElementById("themeslider");
-        var maintheme = document.getElementById("activetheme");
-        if (checkBox.checked == false){
-            secondTheme.style.opacity = "1";
-            secondTheme.style.maxHeight = "200px";
-            secondTheme.style.overflow = "visible";
-            secondTheme.style.padding = "20px 0px 10px 0px";
-            secondTheme.style.transition = "0.4s";
-            slider.style.paddingBottom = "10px";
-            maintheme.setAttribute("value", "unchecked");
-            
-        } else {
-            secondTheme.style.opacity = "0";
-            secondTheme.style.maxHeight = "0px";
-            secondTheme.style.overflow = "hidden";
-            secondTheme.style.padding = "0px";
-            slider.style.paddingBottom = "0px"
-            maintheme.setAttribute("value", "checked");
-        }
-        return;
-      };
-
-     
-
-
-
-
-
-
-
-
-
-
-        (function ($, root, undefined) {
-	
-            $(function () {
-                
-                'use strict';
-                
-                // DOM ready, take it away
-
-                $(".remove").ajaxForm({url: '#recipeareaid', type: 'post'});
-
-                
-            });
-
-            
-            
-        })(jQuery, this);
-
-
-        
-
+    };
+    
 // Tämä funktion hakee ainekset taulukkoon luotaessa uutta reseptiä
 var number = 1;
 
@@ -235,7 +242,7 @@ function addIngredient() {
     };
 
 
-    // Tämä funktion hakee vaiheet taulukkoon luotaessa uutta reseptiä
+// Tämä funktion hakee vaiheet taulukkoon luotaessa uutta reseptiä
 var numberphase = 1;
 
 function addPhase() {
@@ -288,7 +295,6 @@ function addPhase() {
         document.getElementById('amountofshoplistrows').value++;
         };
 
-
         // Tämä laskee ostoslistan tuotteiden lukumäärän valikkoon
 
         var amount = document.getElementById("shoplistid").childElementCount;
@@ -297,7 +303,7 @@ function addPhase() {
         } else {
             document.getElementById("amountofshoprows").style.display = "block";
             document.getElementById("listamountfooterid").innerHTML = amount;
-        }
+        };
 
 
 
@@ -318,3 +324,17 @@ function addPhase() {
          
 
             
+document.getElementById("todesktop").addEventListener("click", desktopSize);
+    function desktopSize() {
+        document.getElementById("devicescale").classList.remove("tablet-device");
+    };
+
+document.getElementById("totablet").addEventListener("click", tabletSize);
+    function tabletSize() {
+        document.getElementById("devicescale").classList.add("tablet-device");
+    };
+
+
+    
+
+                
